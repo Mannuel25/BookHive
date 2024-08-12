e44c3a2abcfc62cdb414c414a3e62c9f81d568c5
mannuel25 <oluwasegunprosperity@gmail.com>
2024-08-12 21:58:48 +0100

    chore: function to generate new access token, using provided refresh_token

--- bookhiveConfig/utils.py
+++ bookhiveConfig/utils.py
@@ -1,11 +1,13 @@
 from django.http import JsonResponse
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+
 
 class CustomResponse:
     """A custom response handler for every response sent by the server"""
 
     @staticmethod
-    def success(data=None, message="", status=200) -> JsonResponse:
+    def success(data=None, message="success", status=200) -> JsonResponse:
         response_data = {
             "status": "success",
             "data": data or [],
@@ -17,7 +19,7 @@ class CustomResponse:
         )
 
     @staticmethod
-    def failed(data=None, message="", status=400) -> JsonResponse:
+    def failed(data=None, message="failed", status=400) -> JsonResponse:
         response_data = {
             "status": "failed",
             "data": data or [],
@@ -28,10 +30,27 @@ class CustomResponse:
             status=status
         )
 
-# an utility function to create JWT tokens
+
+# an utility function to create JWT for a user
 def generate_user_token(user):
     refresh = RefreshToken.for_user(user)
     return {
         "access": str(refresh.access_token),
         "refresh": str(refresh)
     }
+
+
+# an utility function used to generate a new access 
+# token using the provided refresh token
+def refresh_access_token(refresh_token: str):
+    try:
+        # generate a new access token
+        refresh = RefreshToken(refresh_token)
+        access_token = str(refresh.access_token)
+        return {
+            "access": access_token,
+            "refresh": str(refresh)
+        }
+    except (InvalidToken, TokenError) as e:
+        raise ValueError(f"Token error: {str(e)}")
+
